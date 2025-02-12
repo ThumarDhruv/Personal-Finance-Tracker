f07a5e8cf4c1e9644fb11f36911246bacf19bd88
16D06 <121073997+16D06@users.noreply.github.com>
2025-02-12 09:31:10 +0530

    detail-page

--- src/components/TransactionDetails.tsx
+++ src/components/TransactionDetails.tsx
@@ -1,12 +1,16 @@
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../types';
+import { deleteTransaction } from '../store/transactionsSlice';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function TransactionDetails() {
   const { id } = useParams();
   const transaction = useSelector((state: RootState) => 
     state.transactions.transactions.find(t => t.id === id)
   );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   if (!transaction) {
     return <div>Transaction not found</div>;
@@ -43,6 +47,26 @@ export default function TransactionDetails() {
             </dd>
           </div>
         </dl>
+      </div>
+      <div className="flex gap-4 mt-6">
+        <Link to="/transactions" className="px-4 py-2 border rounded-md">
+          &larr; Back
+        </Link>
+        <button
+          onClick={() => navigate(`/transaction/${transaction.id}/edit`)}
+          className="px-4 py-2 bg-blue-500 text-white rounded-md"
+        >
+          Edit
+        </button>
+        <button
+          onClick={() => {
+            dispatch(deleteTransaction(transaction.id));
+            navigate('/transactions');
+          }}
+          className="px-4 py-2 bg-red-500 text-white rounded-md"
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
